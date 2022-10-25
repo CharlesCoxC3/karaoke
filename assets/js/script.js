1580b0dc6bfbec057703882df82d79299a612aa0
Christian Areas <me@areas.me>
2022-10-25 01:02:14 -0700

    Added list of top 10 tracks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,12 @@
 let baseURL = "https://api.musixmatch.com/ws/1.1/"
 let apiKey = "&apikey=53800ed531eed893e70b433586eb11fb"
 
-// Get Top Tracks
+// Get Top Tracks (nice to have)
 function getTopTracks() {
+  // Elements
+  let topTracksList = document.getElementById("top-tracks-list")
+
+  // Endpoint
   let endpoint = "chart.tracks.get" // Docs: https://developer.musixmatch.com/documentation/api-reference/track-chart-get
 
   // Parameters
@@ -28,32 +32,39 @@ function getTopTracks() {
       return response.json()
     })
     .then(function (data) {
-      console.log(data) // response
       console.log(data.message.body.track_list) // ** top tracks
+
+      for (let i = 0; i < data.message.body.track_list.length; i++) {
+        // Create a list item for each track.
+        let listItem = document.createElement("li")
+        // Add the track name to each list item.
+        listItem.textContent = "“" + data.message.body.track_list[i].track.track_name + "”" + " by " + data.message.body.track_list[i].track.artist_name
+        // Add the list item to the list.
+        topTracksList.appendChild(listItem)
+      }
     })
 
 }
+
+// function getLyrics() {
+//   let endpoint = "track.lyrics.get" // Docs: https://developer.musixmatch.com/documentation/api-reference/track-lyrics-get
+  
+//   // Parameters
+//   let track = "&track_id=248716261" // ** temp (will get this from search later)
+  
+//   // Request URL
+//   let requestUrl = baseURL + endpoint + "?" + apiKey + track
+
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json()
+//     })
+//     .then(function (data) {
+//       console.log(data) // ** response
+//       console.log(data.message.body.lyrics.lyrics_body) // ** track lyrics
+//     })
+  
+// }
 
 getTopTracks()
-
-function getLyrics() {
-  let endpoint = "track.lyrics.get" // Docs: https://developer.musixmatch.com/documentation/api-reference/track-lyrics-get
-  
-  // Parameters
-  let track = "&track_id=248716261" // ** temp (will get this from search later)
-  
-  // Request URL
-  let requestUrl = baseURL + endpoint + "?" + apiKey + track
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      console.log(data) // ** response
-      console.log(data.message.body.lyrics.lyrics_body) // ** track lyrics
-    })
-  
-}
-
-getLyrics()
+// getLyrics()
