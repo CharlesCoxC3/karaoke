afcf0722544b215bf3f7db5b13972aeb5fa7485e
Christian Areas <me@areas.me>
2022-10-25 15:53:14 -0700

    Added buttons and track IDs and listners to buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-
 // Build Results UI and Get Lyrics
 // ==================================================
-// Breakdown:
-// 1. Take results from search and create a list.
+// To-Do
+// 1. Take results from search and create a list. √
 // 2. When the user clicks an item in the list, call track.lyrics.get.
 // 3. Save the response somehow.
 
 let baseURL = "https://api.musixmatch.com/ws/1.1/"
 let apiKey = "&apikey=53800ed531eed893e70b433586eb11fb"
 
-// Get Top Tracks (nice to have)
+// Get Top Tracks
 function getTopTracks() {
   // Elements
   let topTracksList = document.getElementById("top-tracks-list")
@@ -32,13 +31,24 @@ function getTopTracks() {
       return response.json()
     })
     .then(function (data) {
-      console.log(data.message.body.track_list) // ** top tracks
-
       for (let i = 0; i < data.message.body.track_list.length; i++) {
         // Create a list item for each track.
         let listItem = document.createElement("li")
-        // Add the track name to each list item.
-        listItem.textContent = "“" + data.message.body.track_list[i].track.track_name + "”" + " by " + data.message.body.track_list[i].track.artist_name
+        
+        // Create a button for each track.
+        let getLyricsButton = document.createElement("button")
+        getLyricsButton.classList.add("get-lyrics-button")
+        getLyricsButton.textContent = "Get Lyrics"
+        getLyricsButton.setAttribute("data-track-id", data.message.body.track_list[i].track.track_id)
+        getLyricsButton.addEventListener("click", getLyrics)
+        
+        // Add the track name, artist, and button to each list item.
+        listItem.textContent =
+          "“" + data.message.body.track_list[i].track.track_name + "”" +
+          " by " +
+          data.message.body.track_list[i].track.artist_name
+        listItem.appendChild(getLyricsButton)
+
         // Add the list item to the list.
         topTracksList.appendChild(listItem)
       }
@@ -46,25 +56,28 @@ function getTopTracks() {
 
 }
 
-// function getLyrics() {
-//   let endpoint = "track.lyrics.get" // Docs: https://developer.musixmatch.com/documentation/api-reference/track-lyrics-get
-  
-//   // Parameters
-//   let track = "&track_id=248716261" // ** temp (will get this from search later)
-  
-//   // Request URL
-//   let requestUrl = baseURL + endpoint + "?" + apiKey + track
-
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json()
-//     })
-//     .then(function (data) {
-//       console.log(data) // ** response
-//       console.log(data.message.body.lyrics.lyrics_body) // ** track lyrics
-//     })
-  
-// }
-
 getTopTracks()
-// getLyrics()
+
+// Get Lyrics
+function getLyrics() {
+  // Elements
+
+  // Endpoint
+  let endpoint = "track.lyrics.get" // Docs: https://developer.musixmatch.com/documentation/api-reference/track-lyrics-get
+  
+  // Parameters
+  let track = "&track_id=248716261" // ** placeholder (will get this dynamically later)
+  
+  // Request URL
+  let requestUrl = baseURL + endpoint + "?" + apiKey + track
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log(data.message.body.lyrics.lyrics_body) // ** track lyrics
+
+    })
+  
+}
